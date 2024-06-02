777d51364fb3ddf5d7208a840eb4a30e9c2e110f
haniyamughal <160358992+haniyamughal@users.noreply.github.com>
2024-06-02 14:45:04 +0500

    Add files via upload
    //  adding an ATM-Machine project

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import inquirer and chalk Packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Display a colourfull Welcome message
+console.log(chalk.bold.cyanBright("\n \t\t Word Counter - by Haniya"));
+console.log("=".repeat(60));
+// Prompt the user to enter a Sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+// Trimmimng the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+// Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
